8ebc8bc536d6a5c04208f4bedda970762b0ae0f7
X-Anas-X <anas.zain1996@gmail.com>
2020-06-29 22:10:49 +0300

    Updates

---
+++ precache-manifest.42d6b68187fc5d64feb14654625d1bb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1d9936978fa00765d8dcdbad596b4e",
+    "url": "/resty-deployment/index.html"
+  },
+  {
+    "revision": "4031379fea2991a34e7e",
+    "url": "/resty-deployment/static/css/main.68ad8343.chunk.css"
+  },
+  {
+    "revision": "d590bc0a0fb725b1e2e3",
+    "url": "/resty-deployment/static/js/2.8e30270d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty-deployment/static/js/2.8e30270d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4031379fea2991a34e7e",
+    "url": "/resty-deployment/static/js/main.f341366f.chunk.js"
+  },
+  {
+    "revision": "8813228d367f869cee41",
+    "url": "/resty-deployment/static/js/runtime-main.4f98a933.js"
+  }
+]);
